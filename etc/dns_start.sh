98c0e91cfd511d243be5978adec136e626fad8fb
gjl3189 <1559434154@qq.com>
2017-04-09 08:35:08 +0800

    Update dns_start.sh

--- etc/dns_start.sh
+++ etc/dns_start.sh
@@ -3,3 +3,9 @@ ps -ef| grep passivedns | awk '{print $2}' | xargs kill -9
 source /root/.bash_profile
 mv /usr/local/cc-shadowsocks/statistics/dns_access.log /usr/local/cc-shadowsocks/statistics/logs/`date "+%Y-%m-%d_%H:0:0_dns_access.log"`
 /usr/local/cc-shadowsocks/statistics/passivedns -D -i eth0 -l /usr/local/cc-shadowsocks/statistics/dns_access.log &
+
+#delete log
+logdir=/usr/local/cc-shadowsocks/statistics/logs/
+filename=`date -d "-15 day" +%Y-%m-%d`
+delfile=$logdir"/"$filename"*.log"
+rm -rf $delfile
