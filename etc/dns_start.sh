c42686033c5f5ab1d272386c55710fc867ce3bdf
gjl3189 <1559434154@qq.com>
2017-04-11 09:33:45 +0800

    Update dns_start.sh

--- etc/dns_start.sh
+++ etc/dns_start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ps -ef| grep passivedns | awk '{print $2}' | xargs kill -9
 source /root/.bash_profile
-ipip=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
+ipip=`cat /usr/local/cc-shadowsocks/statistics/ip.txt`
 dfile=`date "+%Y-%m-%d_%H-0-0"`
 mv /usr/local/cc-shadowsocks/statistics/dns_access.log /usr/local/cc-shadowsocks/statistics/logs/${dfile}_${ipip}_dns_access.log
 cd /usr/local/cc-shadowsocks/statistics/logs/
